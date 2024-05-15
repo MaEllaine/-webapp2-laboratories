fdb62f750281e27f3b0b23dfb6effd2992f4d430
MaEllaine <144407165+MaEllaine@users.noreply.github.com>
2024-05-15 19:30:11 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,41 @@
+<?php
+
+#TASK 1
+$name = "Eleng";
+$age = 20;
+$address = "Highview Hills Sampaloc Apalit Pampanga";
+
+#TASK 2
+//$hobbies = ["Watching movies", "Eating", "Sleeping", "Playing games"]; 
+//$pet_peeve = "Lending something to someone who borrows things but never return that thing.";
+
+echo $name ."<br>";
+echo $age ."<br>";
+echo $address ."<br>";
+//var_dump($hobbies) ."<br>";
+//echo "<br>". $pet_peeve ;
+
+echo "<br>";
+
+#TASK 3
+define('NAME' , 'Eleng');
+define('AGE' , 20);
+define('ADDRESS' , 'Highview Hills Sampaloc Apalit Pampanga');
+define('HOBBIES', ['Watching movies', 'Eating', 'Sleeping', 'Playing games']);
+define('PET_PEEVE', 'Lending something to someone who borrows things but never return that thing.');
+
+#TASK 4
+var_dump(NAME);
+var_dump(AGE);
+var_dump(ADDRESS);
+var_dump(HOBBIES);
+var_dump(PET_PEEVE);
+
+#TASK 5
+
+/*
+This is my pet peeve because it's really annoying for me when someone borrows my things or belongings and never return it at all. 
+I'm not a selfish person but gosh, i hate it when I have to ask for my things back when I shouldn't have to!  
+*/
+
+?>
